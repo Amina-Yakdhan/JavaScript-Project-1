941135dda1c57faccff351c214ee1e05d94fb167
Amina-Yakdhan <yakdamina29@gmail.com>
2026-06-27 23:57:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//Level 1 : Bitwise Operations
+function BitwiseOperations(a,b){ 
+    let BitwiseAND=(a&b);
+    let BitwiseOR=(a|b);
+    let BitwiseXOR=(a^b);
+    return [BitwiseAND,BitwiseOR,BitwiseXOR]
+}
+//level 2 :A Redundant Function
+function Redundant("str"){
+return function();
+return(str);
+}
+//level 3 :Level 3 : Get Notes Distribution
+function Notes Distribution
+var Notes=[1, 2, 3, 4, 5];
+var validNotes=Notes.filter(function(Notes){
+    if(note>1 && note<5){return true;} else{return false;}
+});
+console.log(validNotes)
